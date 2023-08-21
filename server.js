b21ed498173d79388f54c6a5a358eb840e32ed53
Paulo Lima <limapaulo.dev@gmail.com>
2023-08-21 13:44:39 -0300

    node mysql

--- server.js
+++ server.js
@@ -1,57 +1,64 @@
-const express = require('express')
+const express = require('express');
+const mySQL = require('mysql2');
 
-const bodyParser = require('body-parser')
-const usersDB = require('./public/data/users.json')
+const mysqlAuthOpt = { host: 'localhost', user: 'root', password: 'mysqlpw'}
+
+const conn = mySQL.createConnection(mysqlAuthOpt);
+
+conn.connect(err => {
+    if(err) throw err
+    console.log("DB Conectado")
+});
+
+const bodyParser = require('body-parser');
+const usersDB = require('./public/data/users.json');
 
 const app = express();
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-Number("3.14");
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-    console.log("req http get index.html");
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+  console.log('req http get index.html');
 });
 
-app.get("/processar_dados", (req, res) => {
-    res.send(`Bem vindo: ${req.query.nome}, sua idade é:  ${req.query.idade}.`);
+app.get('/processar_dados', (req, res) => {
+  res.send(`Bem vindo: ${req.query.nome}, sua idade é:  ${req.query.idade}.`);
 });
 
-app.post("/processar_dados", (req, res) => {
-    response = {
-        nome: req.body.nome,
-        idade: req.body.idade
-    }
-    res.end(JSON.stringify(response))
+app.post('/processar_dados', (req, res) => {
+  response = {
+    nome: req.body.nome,
+    idade: req.body.idade,
+  };
+  res.end(JSON.stringify(response));
 
-    console.log(req.body)
+  console.log(req.body);
 });
 
-app.post("/login", (req, res) => {
-    response = {
-        username: req.body.username,
-        password: req.body.password
-    }   
+app.post('/login', (req, res) => {
+  response = {
+    username: req.body.username,
+    password: req.body.password,
+  };
 
-    console.log(req.body)
-    console.log(response)
-    console.log(usersDB)
+  console.log(req.body);
+  console.log(response);
+  console.log(usersDB);
 
-    let userFound = false
+  let userFound = false;
 
-    for (let i = 0; i < usersDB.users.length; i++) {
-        if(usersDB.users[i].username == response.username && usersDB.users[i].password == response.password){
-            res.end(response.username + "está logado!")
-            isUser = true
-            break
+  for (let i = 0; i < usersDB.users.length; i++) {
+    if (usersDB.users[i].username == response.username && usersDB.users[i].password == response.password) {
+      res.end(response.username + 'está logado!');
+      isUser = true;
+      break;
     }
-    } if(!userFound){
-        console.log("Usuário ou senha inválido")
-        res.end("Usuário ou senha inválido")
-    }
+  }
+  if (!userFound) {
+    console.log('Usuário ou senha inválido');
+    res.end('Usuário ou senha inválido');
+  }
 });
 
 app.listen(8080);
-
